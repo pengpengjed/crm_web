d3ee919afb6b3fcf0e9a98038ea0a9cfac788c78
zhangyupeng <zhangyupeng@haotaisoft.com>
2024-08-28 14:11:09 +0800

    Revert "feat: 同步crm-web"
    This reverts commit 19ffc014b09a94b906f13036aac2ca7e6d3b9500.

--- build/cdn.ts
+++ build/cdn.ts
@@ -45,6 +45,11 @@ export const cdn = importToCDN({
       name: "dayjs",
       var: "dayjs",
       path: "dayjs.min.js"
+    },
+    {
+      name: "echarts",
+      var: "echarts",
+      path: "echarts.min.js"
     }
   ]
 });
--- build/plugins.ts
+++ build/plugins.ts
@@ -1,7 +1,7 @@
 import { cdn } from "./cdn";
 import vue from "@vitejs/plugin-vue";
 import { pathResolve } from "./utils";
-// import { viteBuildInfo } from "./info";
+import { viteBuildInfo } from "./info";
 import svgLoader from "vite-svg-loader";
 import type { PluginOption } from "vite";
 import checker from "vite-plugin-checker";
@@ -11,29 +11,20 @@ import { configCompressPlugin } from "./compress";
 import removeNoMatch from "vite-plugin-router-warn";
 import { visualizer } from "rollup-plugin-visualizer";
 import removeConsole from "vite-plugin-remove-console";
-// import { themePreprocessorPlugin } from "@pureadmin/theme";
-// import { genScssMultipleScopeVars } from "../src/layout/theme";
-// import { vitePluginFakeServer } from "vite-plugin-fake-server";
-import consola from "consola";
-
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
-import { createHtmlPlugin } from "vite-plugin-html";
+import { themePreprocessorPlugin } from "@pureadmin/theme";
+import { genScssMultipleScopeVars } from "../src/layout/theme";
+import { vitePluginFakeServer } from "vite-plugin-fake-server";
 
 export function getPluginsList(
   VITE_CDN: boolean,
-  VITE_COMPRESSION: ViteCompression,
-  VITE_APP_TITLE: string
+  VITE_COMPRESSION: ViteCompression
 ): PluginOption[] {
   const lifecycle = process.env.npm_lifecycle_event;
-  const plugins: PluginOption[] = [
+  return [
     vue(),
+    // jsx、tsx语法支持
     vueJsx(),
     checker({
-      overlay: {
-        initialIsOpen: false
-      },
       typescript: true,
       vueTsc: true,
       eslint: {
@@ -43,37 +34,38 @@ export function getPluginsList(
       terminal: false,
       enableBuild: false
     }),
+    // 按下Command(⌘)+Shift(⇧)，然后点击页面元素会自动打开本地IDE并跳转到对应的代码位置
     Inspector(),
-    // viteBuildInfo(),
+    viteBuildInfo(),
+    /**
+     * 开发环境下移除非必要的vue-router动态路由警告No match found for location with path
+     * 非必要具体看 https://github.com/vuejs/router/issues/521 和 https://github.com/vuejs/router/issues/359
+     * vite-plugin-router-warn只在开发环境下启用，只处理vue-router文件并且只在服务启动或重启时运行一次，性能消耗可忽略不计
+     */
     removeNoMatch(),
-    /* vitePluginFakeServer({
+    // mock支持
+    vitePluginFakeServer({
       logger: false,
       include: "mock",
       infixName: false,
       enableProd: true
-    }), */
+    }),
+    // 自定义主题
+    themePreprocessorPlugin({
+      scss: {
+        multipleScopeVars: genScssMultipleScopeVars(),
+        extract: true
+      }
+    }),
+    // svg组件化支持
     svgLoader(),
     VITE_CDN ? cdn : null,
     configCompressPlugin(VITE_COMPRESSION),
+    // 线上环境删除console
     removeConsole({ external: ["src/assets/iconfont/iconfont.js"] }),
+    // 打包分析
     lifecycle === "report"
       ? visualizer({ open: true, brotliSize: true, filename: "report.html" })
-      : null,
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    }),
-    createHtmlPlugin({
-      inject: {
-        data: {
-          title: VITE_APP_TITLE
-        }
-      }
-    })
+      : (null as any)
   ];
-
-  consola.success("Loaded plugins:", plugins);
-  return plugins.filter(Boolean);
 }
